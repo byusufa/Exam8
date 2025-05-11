19a07c1661f0cb0ba65f431cb6953f6aff88c8df
byusufa <jozef12079104293@gmail.com>
2025-05-11 23:54:05 +0500

    CustomUserDetailsService commit

---
+++ src/main/java/uz/pdp/exam8/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package uz.pdp.exam8.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import uz.pdp.exam8.repo.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+
+}
