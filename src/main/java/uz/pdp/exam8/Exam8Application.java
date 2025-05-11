2f619a51441cb951272323f8b5589af1a3479deb
byusufa <jozef12079104293@gmail.com>
2025-05-12 00:13:44 +0500

    Exam8Application.java commit

---
+++ src/main/java/uz/pdp/exam8/Exam8Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.exam8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam8Application.class, args);
+    }
+
+}
